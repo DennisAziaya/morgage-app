13fc2ba730b1011c6901406b4a016c48181eb735
Dennis Aziaya Kanyugira <aziayadennis@gmail.com>
2021-12-15 10:17:28 +0300

    Personal Details implementation  completed

--- src/components/steps/PersonalDetails.js
+++ src/components/steps/PersonalDetails.js
@@ -1,102 +1,208 @@
 import * as React from 'react';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
+import {useState} from "react";
+import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import {Container} from "@mui/material";
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import {BlockSharp, Send,  Visibility, VisibilityOff} from "@mui/icons-material";
 
-const PersonalDetails = () => {
+import IntlTelInput from 'react-intl-tel-input';
+import 'react-intl-tel-input/dist/main.css'
+import {FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput} from "@mui/material";
+import useForm from "../useForm";
+
+
+const PersonalDetails = ({ activeStep, steps, handleNext }) => {
+
+
+    const stateSchema = {
+        firstName: {value: "", error: ""},
+        sirName: {value: "", error: ""},
+        physicalAddress: {value: "", error: ""},
+        state: {value: "", error: ""},
+        postalCode: {value: "", error: ""},
+        country: {value: "", error: ""}
+    }
+
+    const stateValidatorSchema = {
+        firstName: {
+            required: true,
+            validator: {
+                func: value => /^([A-Za-z][A-Za-z'-])+([A-Za-z][A-Za-z'-])+([A-Za-z][A-Za-z'-])*/.test(value),
+                error: "First name must be more than 3 characters"
+            }
+        },
+        sirName: {
+            required: true,
+            validator: {
+                func: value => /^([A-Za-z][A-Za-z'-])+([A-Za-z][A-Za-z'-])+([A-Za-z][A-Za-z'-])*/.test(value),
+                error: "Sirname must be more than 3 characters"
+            }
+        },
+        physicalAddress: {
+            required: true,
+            validator: {
+                func: value => /^([A-Za-z][A-Za-z'-])+([A-Za-z][A-Za-z'-])+([A-Za-z][A-Za-z'-])*/.test(value),
+                error: "Address must be more than 3 characters"
+            }
+        },
+        state: {
+            required: true,
+            validator: {
+                func: value => /^([A-Za-z][A-Za-z'-])+([A-Za-z][A-Za-z'-])+([A-Za-z][A-Za-z'-])+([A-Za-z][A-Za-z'-])*/.test(value),
+                error: "State must be more than 4 characters"
+            }
+        },
+        postalCode: {
+            required: true,
+            validator: {
+                func: value => /^([A-Za-z][A-Za-z'-])+([A-Za-z][A-Za-z'-]+)*/.test(value),
+                error: "First name must be more than 1 characters"
+            }
+        },
+        country: {
+            required: true,
+            validator: {
+                func: value => /^([A-Za-z][A-Za-z'-])+([A-Za-z][A-Za-z'-])+([A-Za-z][A-Za-z'-])+([A-Za-z][A-Za-z'-])*/.test(value),
+                error: "Country must be more than 4 characters"
+            }
+        }
+    }
+
+    const {values, errors, dirty, handleOnChange} = useForm(stateSchema, stateValidatorSchema)
+
+    const {firstName, sirName, physicalAddress, state, postalCode, country} = values
+
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        // eslint-disable-next-line no-console
+    };
+
     return (
         <Container maxWidth="md">
-            <Typography sx={{textAlign: "center", textTransform: "uppercase"}} component="h1" variant="h5" gutterBottom>
-                Personal Details
-            </Typography>
-            <Grid container spacing={2}>
-                <Grid item xs={12} sm={6}>
-                    <TextField
-                        required
-                        id="firstName"
-                        name="firstName"
-                        label="First name"
-                        fullWidth
-                        autoComplete="given-name"
-                        variant="standard"
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField
-                        required
-                        id="lastName"
-                        name="lastName"
-                        label="Last name"
-                        fullWidth
-                        autoComplete="family-name"
-                        variant="standard"
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField
-                        required
-                        id="address1"
-                        name="address1"
-                        label="Physical Address"
-                        fullWidth
-                        autoComplete="home address-line1"
-                        variant="standard"
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField
-                        id="address2"
-                        name="address2"
-                        label="Additional Physical Address "
-                        fullWidth
-                        autoComplete="home address-line"
-                        variant="standard"
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField
-                        required
-                        id="city"
-                        name="city"
-                        label="City"
-                        fullWidth
-                        autoComplete="home address-level"
-                        variant="standard"
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField
-                        id="state"
-                        name="state"
-                        label="State/Province/Region"
-                        fullWidth
-                        variant="standard"
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField
-                        required
-                        id="zip"
-                        name="zip"
-                        label="Zip / Postal code"
-                        fullWidth
-                        autoComplete="home postal-code"
-                        variant="standard"
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField
-                        required
-                        id="country"
-                        name="country"
-                        label="Country"
-                        fullWidth
-                        autoComplete="home country"
-                        variant="standard"
-                    />
-                </Grid>
-            </Grid>
+            <Box
+            >
+                <Typography sx={{textAlign: "center", textTransform: "uppercase"}} component="h1" variant="h5"
+                            gutterBottom>
+                    Personal Information
+                </Typography>
+                <Box component="form" noValidate onSubmit={handleSubmit} sx={{mt: 3}}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} sm={4} md={4} lg={4}>
+                            <TextField
+                                required
+                                fullWidth
+                                id="firstName"
+                                label="First Name"
+                                name="firstName"
+                                value={firstName}
+                                onChange={handleOnChange}
+
+                            />
+                            {errors.firstName && dirty.firstName && (
+                                <Typography variant={"p"} sx={{color: "red", fontWeight: "200"}}>
+                                    {errors.firstName}
+                                </Typography>)}
+                        </Grid>
+                        <Grid item xs={12} sm={4} md={4} lg={4}>
+                            <TextField
+                                required
+                                fullWidth
+                                label="Family Name"
+                                name="sirName"
+                                value={sirName}
+                                onChange={handleOnChange}
+                            />
+                            {errors.sirName && dirty.sirName && (
+                                <Typography variant={"p"} sx={{color: "red", fontWeight: "200"}}>
+                                    {errors.sirName}
+                                </Typography>)}
+                        </Grid>
+                        <Grid item xs={12} sm={4} md={4} lg={4}>
+                            <TextField
+                                required
+                                fullWidth
+                                label="Physical Address"
+                                name="physicalAddress"
+                                value={physicalAddress}
+                                onChange={handleOnChange}
+                            />
+                            {errors.physicalAddress && dirty.physicalAddress && (
+                                <Typography sx={{color: "red", fontWeight: "200"}}>
+                                    {errors.physicalAddress}
+                                </Typography>)}
+                        </Grid>
+                        <Grid item xs={12} sm={4} md={4} lg={4}>
+                            <TextField
+                                required
+                                fullWidth
+                                label="State/Province/Region"
+                                name="state"
+                                value={state}
+                                onChange={handleOnChange}
+                            />
+                            {errors.state && dirty.state && (
+                                <Typography sx={{color: "red", fontWeight: "200"}}>
+                                    {errors.state}
+                                </Typography>)}
+                        </Grid>
+                        <Grid item xs={12} sm={4} md={4} lg={4}>
+                            <TextField
+                                required
+                                fullWidth
+                                label="postalCode"
+                                name="postalCode"
+                                value={postalCode}
+                                onChange={handleOnChange}
+                            />
+                            {errors.postalCode && dirty.postalCode && (
+                                <Typography sx={{color: "red", fontWeight: "200"}}>
+                                    {errors.postalCode}
+                                </Typography>)}
+                        </Grid>
+                        <Grid item xs={12} sm={4} md={4} lg={4}>
+                            <TextField
+                                required
+                                fullWidth
+                                label="country"
+                                name="country"
+                                value={country}
+                                onChange={handleOnChange}
+                            />
+                            {errors.country && dirty.country && (
+                                <Typography sx={{color: "red", fontWeight: "200"}}>
+                                    {errors.country}
+                                </Typography>)}
+                        </Grid>
+                    </Grid>
+                    {
+                        !firstName ||
+                        !sirName ||
+                        !physicalAddress ||
+                        !state ||
+                        !country || postalCode ? (
+                            <Button
+                                disabled
+                                onClick={handleNext}
+                                sx={{mt: 3, mb: 2}} variant={"outlined"} endIcon={<BlockSharp/>}>
+                                {activeStep === steps.length ? "Finnish" : "Next"}
+                            </Button>
+                        ) : (
+                            <Button
+                                sx={{mt: 3, mb: 2}}
+                                variant={"outlined"}
+                                onClick={handleNext}
+                                endIcon={<Send/>}>
+                                {activeStep === steps.length ? "Finnish" : "Next"}
+                            </Button>)
+                    }
+                </Box>
+            </Box>
         </Container>
     );
 }
